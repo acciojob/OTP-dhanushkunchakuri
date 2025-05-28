ce2e2bba5435844125b3bcf507184fdde89c5865
Dhanush Kunchakuri <132380514+dhanushkunchakuri@users.noreply.github.com>
2025-05-28 14:15:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-//your JS code here. If required.
+const inputs = document.querySelectorAll(".code-container input");
+
+inputs.forEach((input, index) => {
+  input.addEventListener("input", (e) => {
+    const value = e.target.value;
+    if (/^\d$/.test(value)) {
+      if (index < inputs.length - 1) {
+        inputs[index + 1].focus();
+      }
+    } else {
+      input.value = "";
+    }
+  });
+
+  input.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace") {
+      if (input.value === "" && index > 0) {
+        inputs[index - 1].focus();
+      }
+    }
+  });
+
+  input.addEventListener("paste", (e) => {
+    e.preventDefault();
+    const paste = e.clipboardData.getData("text").replace(/\D/g, "").slice(0, inputs.length);
+    paste.split("").forEach((digit, i) => {
+      if (inputs[i]) inputs[i].value = digit;
+    });
+    if (paste.length < inputs.length) {
+      inputs[paste.length].focus();
+    } else {
+      inputs[inputs.length - 1].focus();
+    }
+  });
+});
